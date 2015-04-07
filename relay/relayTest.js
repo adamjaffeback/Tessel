14ea42ee00fc519a2ebd94b7761d0f23b1f1d8c0
Adam Back <adb33@email.arizona.edu>
2015-04-07 15:37:43 -0700

    Led 0 corresponds to relay.

--- relay/relayTest.js
+++ relay/relayTest.js
@@ -9,11 +9,11 @@ relay.on('ready', function relayReady () {
   setInterval(function toggle() {
     // Toggle relay channel 1
     relay.toggle(1, function toggleOneResult(err) {
-      if (err) console.log("Err toggling 1", err);
-    });
-    // Toggle relay channel 2
-    relay.toggle(2, function toggleTwoResult(err) {
-      if (err) console.log("Err toggling 2", err);
+      if (err) {
+        console.log("Err toggling 1", err) 
+      } else {
+        tessel.led[0].toggle();
+      }
     });
   }, 2000); // Every 2 seconds (2000ms)
 });
